4a3ee7c92785a3aabd1b3f6cb2b11cf4b2f6af0c
Remkus de Vries <remkus@forsite.nu>
2013-12-09 18:12:47 +0100

    Bumping version to 2.0.0

--- genesis-translations.php
+++ genesis-translations.php
@@ -9,7 +9,7 @@
  * Plugin URI: http://remkusdevries.com/plugins/genesis-translations/
  * Description: This plugin translates the Genesis Framework into one of the available languages.
  * Author: Remkus de Vries
- * Version: 1.9.3
+ * Version: 2.0.0
  * Author URI: http://remkusdevries.com/
  * License: GPLv2
  * Text Domain: genesis-translations
