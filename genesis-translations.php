fecb5f16249d911f919f8565e198c3fd0628185d
Remkus de Vries <remkus@forsite.nu>
2013-12-11 09:29:14 +0100

    Fixing get_theme_data notice

--- genesis-translations.php
+++ genesis-translations.php
@@ -49,17 +49,7 @@ load_plugin_textdomain( 'genesis-translations', false, 'genesis-translations/lan
  * @param string  $length Any whole integer
  * @return string
  */
-function fst_genesis_translations_version_check( $str, $length=10 ) {
 
-    if ( strlen( $str ) > $length ) {
-        return substr( $str, 0, $length );
-
-    } else {
-        $res = $str;
-    }
-
-    return $res;
-}
 
 register_activation_hook( __FILE__, 'fst_genesis_translations_activation_check' );
 /**
@@ -68,22 +58,27 @@ register_activation_hook( __FILE__, 'fst_genesis_translations_activation_check' 
  * @author Nathan Rice, Remkus de Vries
  * @uses fst_genesis_translations_activation_check()
  * @since 1.0
- * @version 1.0
+ * @version 2.0.2
  */
 function fst_genesis_translations_activation_check() {
 
-    $latest = '1.7';
+    // Find Genesis Theme Data
+    $theme = wp_get_theme( 'genesis' );
 
-    $theme_info = get_theme_data( get_template_directory() . '/style.css' );
+    // Get the version
+    $version = $theme->get( 'Version' );
 
+    // Set what we consider the minimum Genesis version
+    $minimum_genesis_version = '1.7';
+
+    // Restrict activation to only when the Genesis Framework is activated
     if ( basename( get_template_directory() ) != 'genesis' ) {
         deactivate_plugins( plugin_basename( __FILE__ ) );  // Deactivate ourself
         wp_die( sprintf( __( 'Whoa.. the translation this plugin only works, really, when you have installed the %1$sGenesis Framework%2$s', GTRANS_DOMAIN ), '<a href="http://forsitemedia.net/go/genesis/" target="_new">', '</a>' ) );
     }
 
-    $version = fst_genesis_translations_version_check( $theme_info['Version'], 3 );
-
-    if ( version_compare( $version, $latest, '<' ) ) {
+    // Set a minimum version of the Genesis Framework to be activated on
+    if ( version_compare( $version, $minimum_genesis_version, '<' ) ) {
         deactivate_plugins( plugin_basename( __FILE__ ) );  // Deactivate ourself
         wp_die( sprintf( __( 'Uhm, the thing of it is, you kinda need the %1$sGenesis Framework %2$s%3$s or greater for these translations to make any sense.', GTRANS_DOMAIN ), '<a href="http://forsitemedia.net/go/genesis/" target="_new">', $latest, '</a>' ) );
     }
@@ -98,16 +93,25 @@ add_action( 'genesis_init', 'fst_set_genesis_language_dir', 1 );
  * @author Remkus de Vries, Daan Kortenbach
  * @access public
  * @return void
+ * @since  1.0
+ * @version  2.0.2
  */
 function fst_set_genesis_language_dir() {
 
-    $theme_info = get_theme_data( get_template_directory() . '/style.css' );
-    $version = fst_genesis_translations_version_check( $theme_info['Version'], 3 );
-    $latest = '1.9.1';
+    // Find Genesis Theme Data
+    $theme = wp_get_theme( 'genesis' );
 
+    // Get the version
+    $version = $theme->get( 'Version' );
+
+    // Set what we consider the old translation version
+    $old_translations = '1.9.1';
+
+    // Get root path to translations
     $fstlang = WP_CONTENT_DIR.'/plugins/' .str_replace( basename( __FILE__ ), "", plugin_basename( __FILE__ ) );
 
-    if ( version_compare( $version, $latest, '>' ) ) {
+    // Compare Genesis version with what is set as old translation
+    if ( version_compare( $version, $old_translations, '>' ) ) {
         define( 'GENESIS_LANGUAGES_DIR', $fstlang . 'genesis-translations/' );
     }
     else {
