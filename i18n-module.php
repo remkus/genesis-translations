26020fd9ffcf1904ba43428405953692478c866f
Ryan Hellyer <ryanhellyer@gmail.com>
2016-02-01 16:58:08 +0100

    Setting correct text strings #5

--- i18n-module.php
+++ i18n-module.php
@@ -167,11 +167,11 @@ class ThemeMix_Genesis_Translations_i18n {
 	private function promo_message() {
 		$message = false;
 		if ( $this->translation_exists && $this->translation_loaded && $this->percent_translated < 90 ) {
-			$message = __( 'As you can see, there is a translation of this plugin in %1$s. This translation is currently %3$d%% complete. We need your help to make it complete and to fix any errors. If you would like to help, please visit %4$s!', $this->textdomain );
+			$message = __( 'As you can see, there is a translation of this plugin in %1$s. This translation is currently %3$d%% complete. We need your help to make it complete and to fix any errors. If you would like to help, please visit %4$s!', 'genesis-translations' );
 		} else if ( ! $this->translation_loaded && $this->translation_exists ) {
-			$message = __( 'You\'re using WordPress in %1$s. While %2$s has been translated to %1$s for %3$d%%, it\'s not been shipped with the plugin yet. You can help! Visit %4$s to help complete the translation!', $this->textdomain );
+			$message = __( 'You\'re using WordPress in %1$s. While %2$s has been translated to %1$s for %3$d%%, it\'s not been shipped with the plugin yet. You can help! Visit %4$s to help complete the translation!', 'genesis-translations' );
 		} else if ( ! $this->translation_exists ) {
-			$message = __( 'You\'re using WordPress in a language we don\'t support yet. We\'d love for %2$s to be translated in that language too, but unfortunately, it isn\'t right now. You can change that! Visit %4$s to help translate it!', $this->textdomain );
+			$message = __( 'You\'re using WordPress in a language we don\'t support yet. We\'d love for %2$s to be translated in that language too, but unfortunately, it isn\'t right now. You can change that! Visit %4$s to help translate it!', 'genesis-translations' );
 		}
 
 		$registration_link = sprintf( '<a href="%1$s">%2$s</a>', esc_url( $this->register_url ), esc_html( $this->glotpress_name ) );
@@ -192,7 +192,7 @@ class ThemeMix_Genesis_Translations_i18n {
 			echo '<br /><br />';
 			echo '<div id="i18n_promo_box" style="border:1px solid #ccc;background-color:#fff;padding:10px;max-width:650px;">';
 			echo '<a href="' . esc_url( add_query_arg( array( 'remove_i18n_promo' => '1' ) ) ) . '" style="color:#333;text-decoration:none;font-weight:bold;font-size:16px;border:1px solid #ccc;padding:1px 4px;" class="alignright">X</a>';
-			echo '<h2>' . sprintf( __( 'Translation of %s', $this->textdomain ), $this->plugin_name ) . '</h2>';
+			echo '<h2>' . sprintf( __( 'Translation of %s', 'genesis-translations' ), $this->plugin_name ) . '</h2>';
 			if ( isset( $this->glotpress_logo ) && '' != $this->glotpress_logo ) {
 				echo '<a href="' . $this->register_url . '"><img class="alignright" style="margin:15px 5px 5px 5px;width:200px;" src="' . $this->glotpress_logo . '" alt="' . $this->glotpress_name . '"/></a>';
 			}
@@ -230,7 +230,7 @@ class ThemeMix_Genesis_Translations_i18n {
 		$set = $this->find_or_initialize_translation_details();
 
 		$this->translation_exists = ! is_null( $set );
-		$this->translation_loaded = is_textdomain_loaded( $this->textdomain );
+		$this->translation_loaded = is_textdomain_loaded( 'genesis-translations' );
 
 		$this->parse_translation_set( $set );
 	}
