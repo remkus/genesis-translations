88502e2d985274f5c07f1ca91c1b452e9d617da4
Remkus de Vries <remkus@forsite.nu>
2018-03-21 10:40:34 +0100

    Simplefying the admin page

--- admin-page.php
+++ admin-page.php
@@ -5,7 +5,7 @@ add_action( 'admin_menu', 'thememix_genesis_translations_add_admin_page' );
  * Adds admin page under the "Genesis" menu.
  * Explains how to implement new and improved translations.
  *
- * @author Remkus de Vries, Ryan Hellyer
+ * @author Remkus de Vries
  * @since 2.1.0
  */
 function thememix_genesis_translations_add_admin_page() {
@@ -24,17 +24,16 @@ function thememix_genesis_translations_add_admin_page() {
 /**
  * Outputs the admin page.
  *
- * @author Remkus de Vries, Ryan Hellyer
+ * @author Remkus de Vries
  * @since 2.1.0
  */
 function thememix_genesis_translations_admin_page() {
 	?>
 	<div class="wrap">
-		<h1><?php _e( 'Genesis Translations by ThemeMix', 'genesis-translations' ); ?></h1>
+		<h1><?php _e( 'Genesis Translations', 'genesis-translations' ); ?></h1>
 		<div style="margin-top: 20px;border:1px solid #ccc;background-color:#fff;padding:10px;max-width:650px;">
-			<h3>Important Update!</h3>
-			<p><?php _e( 'Genesis Translations will soon rely on the same mechanism used to provide and update WordPress\'s core, plugins and themes translations files.', 'genesis-translations' ); ?></p>
-			<p><?php _e( '<strong>You can help!</strong> Please take the time to translate this plugin which includes all the strings for the Genesis Framework. We have a tutorial available for anyone wanting to contribute translating the Genesis Framework.', 'genesis-translations' ); ?></p>
+			<h2>Help translate the Genesis Framework in your locale</h2>
+			<p><?php _e( 'Is the translation of Genesis in your language not 100% yet? <strong>You can help!</strong> Please take the time to translate this plugin which includes all the strings for the Genesis Framework. We have a tutorial available for anyone wanting to contribute translating the Genesis Framework.', 'genesis-translations' ); ?></p>
 			<p><?php _e( 'Please visit <a target="_blank" href="https://thmmx.link/gtpage/">our Knowledge Base</a> for instruction on how to help.', 'genesis-translations' ) ?>
 	</div></div><?php
 
